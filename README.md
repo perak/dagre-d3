037938b74385a9a5900c56e2bf327706ed5873e0
Petar Korponaić <petar.korponaic@gmail.com>
2015-05-14 10:33:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+dagre-d3
+========
+
+D3-based renderer for Dagre packaged for meteor.
+
+Exposes `dagreD3` on your client.
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+	summary: "D3-based renderer for Dagre",
+	version: "1.0.0",
+	git: "https://github.com/perak/dagre-d3.git"
+});
+
+// Before Meteor 0.9?
+if(!Package.onUse) Package.onUse = Package.on_use;
+
+Package.onUse(function (api) {
+	if(api.versionsFrom) {
+		api.versionsFrom('METEOR@0.9.0');
+	}
+
+	api.use("d3");
+
+	api.add_files('dagre-d3/dist/dagre-d3.js', "client");
+	api.export('dagreD3', "client");
+});
